d12a3f89e015dc254048433d3a6c062b97eb82be
jonaspetraska7 <jonas.petraska7@gmail.com>
2020-11-29 16:57:15 +0200

    inital MVC project
    MVC CRUD realization

---
+++ Project One/Project One/Controllers/TodoItemsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_One.Models;
+
+
+namespace Project_One.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+
+        private static List<TodoItemModel> Data = new List<TodoItemModel>
+        {
+            new TodoItemModel{Id = 1, Description="asd", Name="Jonas"},
+            new TodoItemModel{Id = 2, Description="asdf", Name="Jonas2"}
+        };
+        // GET: 
+
+        public ActionResult Index()
+        {
+            return View(Data);
+        }
+
+        // GET: TodoItems/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(Data);
+        }
+
+        // GET: TodoItems/Create
+        public ActionResult Create()
+        {
+            return View(Data);
+        }
+
+        // GET: TodoItems/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(Data);
+        }
+
+        // GET: TodoItems/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(Data);
+        }
+
+    }
+}
---
+++ Project One/Project One/Models/TodoItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_One.Models
+{
+    public class TodoItemModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [DefaultValue(3)]
+        [Range(1,5,ErrorMessage ="Number is not in range 1 to 5")]
+        public int Priority { get; set; }
+
+    }
+}
